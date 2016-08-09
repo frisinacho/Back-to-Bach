08d9bc560b696790c7f02f52d856117e40492bd4
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-08-09 18:32:34 +0200

    Adding 'scrub', 'pause' and 'stop' actions

--- Back To Bach/ViewController.swift
+++ Back To Bach/ViewController.swift
@@ -18,6 +18,17 @@ class ViewController: UIViewController {
     @IBAction func adjustVolume(sender: AnyObject) {
     }
     
+    @IBOutlet var scrubSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
